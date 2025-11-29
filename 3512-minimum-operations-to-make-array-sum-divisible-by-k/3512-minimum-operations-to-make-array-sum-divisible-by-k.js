f7c5af641f37e7a65585554eef9b266a2ad01471
JOYAL JOSE <jhub0866@gmail.com>
2025-11-29 16:07:39 +0530

    Time: 2 ms (36.26%), Space: 58.3 MB (19.47%) - LeetHub

---
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    let sum = 0;
+    for (let x of nums){
+        sum += x;
+    }
+    return sum % k;
+};
