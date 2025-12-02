3e2b2e5cf9308d491b14bbb77d3926cf46387678
JOYAL JOSE <jhub0866@gmail.com>
2025-12-02 13:20:11 +0530

    Time: 117 ms (53.33%), Space: 96 MB (86.67%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.ts
@@ -0,0 +1,23 @@
+function countTrapezoids(points: number[][]): number {
+     const MOD = BigInt(1_000_000_007);
+    const mp = new Map();  
+    let sumPairs = BigInt(0);
+
+    for (let p of points) {
+        let y = p[1];
+        let f = mp.get(y) || 0;
+        sumPairs += BigInt(f);
+        mp.set(y, f + 1);
+    }
+
+    let res = BigInt(0);
+
+    for (let y of mp.keys()) {
+        let f = mp.get(y);
+        let cur = BigInt(f * (f - 1) / 2); 
+        sumPairs -= cur;
+        res += sumPairs * cur;
+    }
+
+    return Number(res % MOD);
+};
