991ed3d6d54f9436356cb7dca9bf43e90b010a89
JOYAL JOSE <jhub0866@gmail.com>
2025-12-08 10:34:39 +0530

    Time: 5 ms (67.31%), Space: 55.8 MB (36.54%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countTriples = function(n) {
+    let count=0
+    for (let a=1;a<=n;a++) {
+        for(let b=1;b<=n;b++){
+            let sum=Math.sqrt((a*a)+(b*b))
+            
+            if(Number.isInteger(sum)==true&& sum<=n){
+                 count++
+            }
+        }
+    }
+    return count
+    
+};
