25f10078d8d40f5239605079027fd63ed2fe547d
JOYAL JOSE <jhub0866@gmail.com>
2025-12-09 13:31:54 +0530

    Time: 303 ms (16.67%), Space: 93 MB (100%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.ts
@@ -0,0 +1,23 @@
+const specialTriplets = (nums: number[]): number => {
+    const MOD = 1e9 + 7;
+    const n = nums.length;
+    let result = 0;
+    const prefixCount = new Map<number, number>();
+    const suffixCount = new Map<number, number>();
+    for (const num of nums) {
+        suffixCount.set(num, (suffixCount.get(num) || 0) + 1);
+    }
+
+    for (const mid of nums) {
+        suffixCount.set(mid, suffixCount.get(mid)! - 1); 
+        const target = mid * 2;
+        const left = prefixCount.get(target) || 0;
+        const right = suffixCount.get(target) || 0;
+
+        result = (result + left * right) % MOD;
+
+        prefixCount.set(mid, (prefixCount.get(mid) || 0) + 1); 
+    }
+
+    return result;
+};
