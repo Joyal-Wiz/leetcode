135a42d590de6241fa51a0f1a1cd987c369f3ed3
JOYAL JOSE <jhub0866@gmail.com>
2025-12-15 10:03:45 +0530

    Time: 3 ms (80%), Space: 66.8 MB (25%) - LeetHub

---
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var getDescentPeriods = function(prices) {
+        let res = prices.length;
+    for(let i = 0; i < prices.length; i++) {
+        let j = i + 1;
+        while (j < prices.length && prices[j - 1] - prices[j] === 1) {j++;}
+        const len = j - i;
+        res += len * (len - 1) / 2;
+        i = j - 1;
+    }
+
+    return res;
+};
