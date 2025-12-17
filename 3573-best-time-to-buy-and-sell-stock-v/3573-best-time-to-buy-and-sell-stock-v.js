db6154351b9db81372063373b214f9817e309451
JOYAL JOSE <jhub0866@gmail.com>
2025-12-17 09:44:31 +0530

    Time: 20 ms (100%), Space: 58.8 MB (100%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.js
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.js
@@ -2,25 +2,24 @@
  * @param {number[]} prices
  * @param {number} k
  * @return {number}
- */
-var maximumProfit = function(prices, k) {
-    const firstPrice = prices[0];
-    const dp = Array(k + 1).fill(null).map(() => ({
-        maxProfit: 0,
-        buyHold: -firstPrice,
-        sellHold: firstPrice
-    }));
+ */var maximumProfit = function (prices, k) {
     const n = prices.length;
-    
-    for (let day = 1; day < n; day++) {
-        const currPrice = prices[day];
-        for (let trans = k; trans > 0; trans--) {
-            const prevProfit = dp[trans - 1].maxProfit;
-            dp[trans].maxProfit = Math.max(dp[trans].maxProfit, dp[trans].buyHold + currPrice, dp[trans].sellHold - currPrice);
-            dp[trans].buyHold = Math.max(dp[trans].buyHold, prevProfit - currPrice);
-            dp[trans].sellHold = Math.max(dp[trans].sellHold, prevProfit + currPrice);
+    const buy = Array(k + 1).fill(-Infinity);
+    const sell = Array(k + 1).fill(-Infinity);
+    const done = Array(k + 1).fill(0);
+
+    for (let price of prices) {
+        for (let t = k; t >= 1; t--) {
+            buy[t] = Math.max(buy[t], done[t - 1] - price);
+            sell[t] = Math.max(sell[t], done[t - 1] + price);
+
+            done[t] = Math.max(
+                done[t],
+                buy[t] + price,
+                sell[t] - price
+            );
         }
     }
-    
-    return dp[k].maxProfit;
+
+    return done[k];
 };
