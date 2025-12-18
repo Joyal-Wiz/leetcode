ef3e9cee0943b0320830d52c286abb8c6f8e9967
JOYAL JOSE <jhub0866@gmail.com>
2025-12-18 16:10:30 +0530

    Time: 1 ms (46.84%), Space: 56 MB (29.74%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var alternatingSum = function(nums) {
+        let count = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (i % 2 === 0)
+                count += nums[i];
+            else
+                count -= nums[i];
+        }
+        return count;
+};
