d5dd27da55d914a6e11279083300753e28960a27
JOYAL JOSE <jhub0866@gmail.com>
2025-12-19 10:59:26 +0530

    Time: 229 ms (50%), Space: 115.7 MB (33.33%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.js
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.js
@@ -5,44 +5,41 @@
  * @return {number[]}
  */
 var findAllPeople = function(n, meetings, firstPerson) {
- meetings.sort((a, b) => a[2] - b[2]);
+        meetings.sort((a, b) => a[2] - b[2]);
 
-const parent = Array.from({ length: n }, (_, i) => i);
-const know = Array(n).fill(false);
-know[0] = know[firstPerson] = true;
+    const parent = Array.from({ length: n }, (_, i) => i);
+    const know = Array(n).fill(false);
+    know[0] = know[firstPerson] = true;
 
-const find = x => parent[x] === x ? x : (parent[x] = find(parent[x]));
-const unite = (a, b) => {
-    const pa = find(a), pb = find(b);
-    if (pa !== pb) parent[pb] = pa;
-};
+    const find = x => parent[x] === x ? x : parent[x] = find(parent[x]);
 
-for (let i = 0; i < meetings.length; ) {
-    const time = meetings[i][2];
-    const people = new Set();
+    const unite = (a, b) => {
+        const pa = find(a), pb = find(b);
+        if (pa !== pb) parent[pb] = pa;
+    };
 
-    let j = i;
-    while (j < meetings.length && meetings[j][2] === time) {
-        const [x, y] = meetings[j];
-        unite(x, y);
-        people.add(x);
-        people.add(y);
-        j++;
+    for (let i = 0; i < meetings.length; ) {
+        const t = meetings[i][2];
+        const temp = [];
+
+        let j = i;
+        while (j < meetings.length && meetings[j][2] === t) {
+            unite(meetings[j][0], meetings[j][1]);
+            temp.push(meetings[j][0], meetings[j][1]);
+            j++;
+        }
+
+        for (const x of temp)
+            if (know[x]) know[find(x)] = true;
+
+        for (const x of temp)
+            know[x] ||= know[find(x)];
+
+        for (const x of temp)
+            parent[x] = x;
+
+        i = j;
     }
-    const secretRoots = new Set();
-    for (const p of people) {
-        if (know[p]) secretRoots.add(find(p));
-    }
 
-    for (const p of people) {
-        if (secretRoots.has(find(p))) know[p] = true;
-    }
-
-    for (const p of people) parent[p] = p;
-
-    i = j;
-}
-
-return know.flatMap((v, i) => v ? i : []);
-
+    return know.map((v, i) => v ? i : -1).filter(v => v !== -1);
 };
