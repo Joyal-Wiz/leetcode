d50761778da7af2349f2f5a2adfa811bd068937a
JOYAL JOSE <jhub0866@gmail.com>
2025-12-19 11:04:42 +0530

    Time: 283 ms (100%), Space: 117.7 MB (100%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.ts
@@ -0,0 +1,45 @@
+function findAllPeople(n: number, meetings: number[][], firstPerson: number): number[] {
+    meetings.sort((a, b) => a[2] - b[2]);
+
+const know = new Array(n).fill(false);
+know[0] = know[firstPerson] = true;
+
+let i = 0;
+
+while (i < meetings.length) {
+    const time = meetings[i][2];
+
+    const graph = new Map();
+    const queue = [];
+
+    let j = i;
+    while (j < meetings.length && meetings[j][2] === time) {
+        const [x, y] = meetings[j];
+
+        if (!graph.has(x)) graph.set(x, []);
+        if (!graph.has(y)) graph.set(y, []);
+
+        graph.get(x).push(y);
+        graph.get(y).push(x);
+
+        j++;
+    }
+    for (const person of graph.keys()) {
+        if (know[person]) queue.push(person);
+    }
+
+    for (let k = 0; k < queue.length; k++) {
+        const u = queue[k];
+        for (const v of graph.get(u)) {
+            if (!know[v]) {
+                know[v] = true;
+                queue.push(v);
+            }
+        }
+    }
+
+    i = j;
+}
+
+return know.map((v, i) => v ? i : -1).filter(v => v !== -1);
+};
