76e5d62d79d7d57284880dcc69d7629f8020319f
JOYAL JOSE <jhub0866@gmail.com>
2025-12-22 09:36:19 +0530

    Time: 9 ms (50%), Space: 57 MB (25%) - LeetHub

---
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var minDeletionSize = function(strs) {
+    const cols = strs[0].length;
+    const rows = strs.length;
+    const dp = Array(cols).fill(1);
+
+    for (let c = 1; c < cols; c++) {
+        for (let j = 0; j < c; j++) {
+            let valid = true;
+            for (let r = 0; r < rows; r++) {
+                if (strs[r][j] > strs[r][c]) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                dp[c] = Math.max(dp[c], dp[j] + 1);
+            }
+        }
+    }
+
+    return cols - Math.max(...dp);
+};
