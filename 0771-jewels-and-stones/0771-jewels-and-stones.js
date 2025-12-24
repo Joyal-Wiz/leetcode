99f65bf463cd996210aeeb0929d483e2cc7a3619
JOYAL JOSE <jhub0866@gmail.com>
2025-12-24 09:41:35 +0530

    Time: 0 ms (100%), Space: 54 MB (92.04%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.js
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -4,8 +4,19 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-   let regex = new RegExp(`[${jewels}]`, "g");
-let count = stones.match(regex)?.length || 0;
 
-return count;
+let count=0;
+for(i=0;i<jewels.length;i++){
+    for(j=0;j<stones.length;j++){
+        if(jewels[i]==stones[j]){
+
+            count++;
+        }
+
+
+    }
+   
+}
+
+ return count;
 };
