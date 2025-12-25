e12acd4cc0fca66aefc387c94dd63e078e92b89c
JOYAL JOSE <jhub0866@gmail.com>
2025-12-25 09:43:43 +0530

    Time: 232 ms (66.67%), Space: 73.3 MB (100%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.ts
@@ -0,0 +1,12 @@
+function maximumHappinessSum(happiness: number[], k: number): number {
+        happiness.sort((a, b) => a - b);
+    let res = 0;
+    let count = 0;
+    for (let i = happiness.length - 1; i >= happiness.length - k; i--) {
+        if (happiness[i] - count > 0) {
+            res += (happiness[i] - count);
+        }
+        count++;
+    }
+    return res;
+};
