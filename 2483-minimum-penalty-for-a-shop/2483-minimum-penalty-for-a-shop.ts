2e3b39b218c941bcf185a535e7b684d489eee262
JOYAL JOSE <jhub0866@gmail.com>
2025-12-26 10:11:18 +0530

    Time: 13 ms (62.96%), Space: 58.7 MB (85.19%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.ts
@@ -0,0 +1,20 @@
+function bestClosingTime(customers: string): number {
+    let pen = 0;
+    const n = customers.length;
+    for (let i = 0; i < n; i++) {
+        if (customers.charCodeAt(i) === 89) pen++; 
+    }
+
+    let best = pen;
+    let ans = 0;
+
+    for (let i = 0; i < n; i++) {
+        pen += (customers.charCodeAt(i) === 89) ? -1 : 1;
+
+        if (pen < best) {
+            best = pen;
+            ans = i + 1;
+        }
+    }
+    return ans;
+};
