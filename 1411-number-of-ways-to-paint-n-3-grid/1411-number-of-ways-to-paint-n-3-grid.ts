de443521494d9292dca5835669e252e15e84a934
JOYAL JOSE <jhub0866@gmail.com>
2026-01-03 09:49:44 +0530

    Time: 3 ms (66.67%), Space: 57.5 MB (66.67%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.ts
@@ -0,0 +1,13 @@
+function numOfWays(n: number): number {
+    const mod=1000000007;
+let a=6, b=6;
+
+for(let i = 2; i <= n; i++){
+  const newA=(2*a+2*b)%mod;
+  const newB=(2*a+3*b)%mod;
+    a=newA;
+    b=newB;
+    
+}
+    return(a+b)%mod;
+};
