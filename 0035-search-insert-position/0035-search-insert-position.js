c9c4566e9aed72525692735b1b465f0e5fda3cc6
JOYAL JOSE <jhub0866@gmail.com>
2026-01-07 13:52:52 +0530

    Time: 0 ms (100%), Space: 55 MB (15.67%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,11 +4,18 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for (let i=0;i<nums.length;i++) {
-        if (nums[i] >= target) {
-            return i;
+    let left = 0;
+    let right = nums.length - 1;
+    while(left <= right){
+        let mid = Math.floor((left + right )/2);
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid]<target){
+            left = mid + 1;
         }
-    }
-    return nums.length;
+        else{
+            right = mid - 1;
+        }
+    }return left
 
 }
