acd94e8f26accc2c873cf7f0175efa27e76f0519
JOYAL JOSE <jhub0866@gmail.com>
2026-01-09 09:31:28 +0530

    Time: 0 ms (100%), Space: 57.1 MB (21.43%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    return hours.filter(i=>i>=target).length;
+    
+};
