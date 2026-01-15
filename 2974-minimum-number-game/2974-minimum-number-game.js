39a5c55682f507e885324546382ef90a54d4c96c
JOYAL JOSE <jhub0866@gmail.com>
2026-01-15 14:59:11 +0530

    Time: 4 ms (31.63%), Space: 58 MB (41.53%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.js
+++ 2974-minimum-number-game/2974-minimum-number-game.js
@@ -3,16 +3,11 @@
  * @return {number[]}
  */
 var numberGame = function(nums) {
-nums.sort((a, b) => a - b); 
-    let result = [];
-
-    for (let i = 0; i < nums.length; i += 2) {
-        let alice = nums[i];
-        let bob = nums[i + 1];
-
-        result.push(bob);
-        result.push(alice);
+    nums.sort((a,b)=>a-b)
+    let arr=[]
+    for(let i=0;i<nums.length;i+=2){
+        arr.push(nums[i+1])
+        arr.push(nums[i])
     }
-
-    return result;
+    return arr
 };
