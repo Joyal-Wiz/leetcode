ba854b158ad86b753b4584edcbf10bf32830886f
JOYAL JOSE <jhub0866@gmail.com>
2026-01-15 14:53:25 +0530

    Time: 0 ms (100%), Space: 57 MB (40.21%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,6 +3,8 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    if(num==0) return 0 
+    if (num === 0) return 0;
     return 1 + (num - 1) % 9;
 };
+console.log(addDigits(38));
+console.log(addDigits(0)); 
