ac929b366d238cf89eaaee5e28d5d42b23a644d1
JOYAL JOSE <jhub0866@gmail.com>
2026-01-25 12:22:33 +0530

    Time: 9 ms (55.74%), Space: 58.4 MB (22.95%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.ts
@@ -0,0 +1,14 @@
+function minimumDifference(nums: number[], k: number): number {
+        if (k === 1) return 0;
+    nums.sort((a, b) => a - b);
+    let minDiff = Infinity;
+
+    for (let i = 0; i <= nums.length - k; i++) {
+        minDiff = Math.min(
+            minDiff,
+            nums[i + k - 1] - nums[i]
+        );
+    }
+    return minDiff;
+
+};
