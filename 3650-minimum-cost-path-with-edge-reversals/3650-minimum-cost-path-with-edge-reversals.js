b5980ffb7a015dc3fc9db86b57a6a0db0f2311f6
JOYAL JOSE <jhub0866@gmail.com>
2026-01-27 14:24:22 +0530

    Time: 588 ms (100%), Space: 125.8 MB (61.54%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.js
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.js
@@ -4,72 +4,34 @@
  * @return {number}
  */
 var minCost = function(n, edges) {
-    const graph = Array.from({ length: n }, () => []);
-    for (const [u, v, w] of edges) {
-        graph[u].push([v, w]);
-        graph[v].push([u, 2 * w]);
+const edgeMap = new Map(); // node -> { node, weight }
+    for (let i = 0; i < n; i++) {
+        edgeMap.set(i, []);
     }
 
-    class MinHeap {
-        constructor() { 
-            this.h = [];
-        }
-        less(a, b) {
-            return a[0] < b[0] || (a[0] === b[0] && a[1] < b[1]);
-        }
-        push(x) {
-            const h = this.h;
-            h.push(x);
-            let i = h.length - 1;
-            while (i > 0) {
-                const p = (i - 1) >> 1;
-                if (this.less(h[p], h[i])) 
-                    break;
-                [h[p], h[i]] = [h[i], h[p]];
-                i = p;
-            }
-        }
-        pop() {
-            const h = this.h;
-            const res = h[0];
-            const last = h.pop();
-            if (h.length) {
-                h[0] = last;
-                let i = 0;
-                while (true) {
-                    let l = i * 2 + 1, r = l + 1, s = i;
-                    if (l < h.length && this.less(h[l], h[s])) s = l;
-                    if (r < h.length && this.less(h[r], h[s])) s = r;
-                    if (s === i) 
-                        break;
-                    [h[s], h[i]] = [h[i], h[s]];
-                    i = s;
-                }
-            }
-            return res;
-        }
-        size() { return this.h.length; }
+    for (var edge of edges) {
+        const [from, to, weight] = edge;
+        edgeMap.get(from).push({ node: to, weight });
+        edgeMap.get(to).push({ node: from, weight: weight * 2 });
     }
 
-    const ans = new Array(n).fill(Infinity);
-    ans[0] = 0;
+    const visited = new Set();
+    const heap = new MinHeap((node) => node.cost);
+    heap.push({ node: 0, cost: 0 });
 
-    const pq = new MinHeap();
-    pq.push([0, 0]); 
+    while (heap.size() > 0) {
+        const { node, cost } = heap.pop();
+        if (visited.has(node)) continue;
+        if (node === n - 1) return cost;
 
-    while (pq.size()) {
-        const [weight, node] = pq.pop();
-        if (weight > ans[node]) 
-            continue;
+        visited.add(node);
 
-        for (const [nd, wt] of graph[node]) {
-            if (wt + weight < ans[nd]) {
-                ans[nd] = wt + weight;
-                pq.push([ans[nd], nd]);
-            }
+        const connectedEdges = edgeMap.get(node);
+        for (var edge of connectedEdges) {
+            heap.push({ node: edge.node, cost: cost + edge.weight})
         }
     }
 
-    return ans[n - 1] === Infinity ? -1 : ans[n - 1];
+    return -1;
 
 };
