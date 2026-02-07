7833951bc49dc63fc6698321a89267b0f8b407d3
JOYAL JOSE <jhub0866@gmail.com>
2026-02-07 10:17:31 +0530

    Time: 26 ms (35.47%), Space: 67.6 MB (82.56%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+
+return Math.min(new Set([...candyType]).size, candyType.length/2)
+
+};
