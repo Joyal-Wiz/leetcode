7ea9233f78e1d143508fdd043216ff56c632009a
JOYAL JOSE <jhub0866@gmail.com>
2026-02-19 17:28:35 +0530

    Time: 279 ms (94.9%), Space: 94.6 MB (39.29%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.ts
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.ts
@@ -1,27 +1,36 @@
 function maximizeSquareArea(m: number, n: number, hFences: number[], vFences: number[]): number {
     const MOD = 1000000007n;
+
     hFences.push(1, m);
     vFences.push(1, n);
+
     hFences.sort((a, b) => a - b);
     vFences.sort((a, b) => a - b);
 
-    const hs = new Set();
+    if (hFences.length > vFences.length) {
+        [hFences, vFences] = [vFences, hFences];
+    }
+
+    const set = new Set<number>();
     for (let i = 0; i < hFences.length; i++) {
         for (let j = i + 1; j < hFences.length; j++) {
-            hs.add(hFences[j] - hFences[i]);
+            set.add(hFences[j] - hFences[i]);
         }
     }
 
-    let s = 0;
+    let maxSide = 0;
+
     for (let i = 0; i < vFences.length; i++) {
         for (let j = i + 1; j < vFences.length; j++) {
             const d = vFences[j] - vFences[i];
-            if (d > s && hs.has(d)) s = d;
+            if (d > maxSide && set.has(d)) {
+                maxSide = d;
+            }
         }
     }
 
-    if (s === 0) return -1;
-    const x = BigInt(s);
-    return Number((x * x) % MOD);
+    if (maxSide === 0) return -1;
 
-};
+    const x = BigInt(maxSide);
+    return Number((x * x) % MOD);
+}
