21e00fb53d748e3360476bb7b6aa7fb7989d25c7
JOYAL JOSE <jhub0866@gmail.com>
2026-02-19 17:32:31 +0530

    Time: 40 ms (91.5%), Space: 82.3 MB (33.6%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.js
+++ 0164-maximum-gap/0164-maximum-gap.js
@@ -3,16 +3,34 @@
  * @return {number}
  */
 var maximumGap = function(nums) {
-   if (nums.length < 2) return 0;
+    const n = nums.length;
+    if (n < 2) return 0;
 
-    nums.sort((a, b) => a - b);
+    const min = Math.min(...nums);
+    const max = Math.max(...nums);
+    
+    if (min === max) return 0;
+
+    const bucketSize = Math.max(1, Math.floor((max - min) / (n - 1)));
+    const bucketCount = Math.floor((max - min) / bucketSize) + 1;
+    const bucketsMin = new Array(bucketCount).fill(Infinity);
+    const bucketsMax = new Array(bucketCount).fill(-Infinity);
+
+    for (let x of nums) {
+        const idx = Math.floor((x - min) / bucketSize);
+        bucketsMin[idx] = Math.min(bucketsMin[idx], x);
+        bucketsMax[idx] = Math.max(bucketsMax[idx], x);
+    }
 
     let maxGap = 0;
+    let prevMax = min;
 
-    for (let i = 1; i < nums.length; i++) {
-        maxGap = Math.max(maxGap, nums[i] - nums[i - 1]);
+    for (let i = 0; i < bucketCount; i++) {
+        if (bucketsMin[i] === Infinity) continue;
+
+        maxGap = Math.max(maxGap, bucketsMin[i] - prevMax);
+        prevMax = bucketsMax[i];
     }
 
     return maxGap;
-
 };
