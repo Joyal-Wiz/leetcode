d2570453db50bfc6c244b6c9cc53c31590299ab6
JOYAL JOSE <jhub0866@gmail.com>
2026-02-21 10:30:49 +0530

    Time: 1 ms (55.44%), Space: 58.3 MB (44.01%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,16 +3,14 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    let arr=[];
-for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        arr.push("FizzBuzz")
-    } else if (i % 3 === 0) {
-        arr.push("Fizz")
-    } else if (i % 5 === 0) {
-        arr.push("Buzz")
-    } else {
-        arr.push(i.toString())
-    }}
-    return arr
+    let result = []
+    for (let i = 1; i<=n; i++){
+        let str = ""
+        if(i % 3 === 0) str += "Fizz"
+        if(i % 5 === 0) str += "Buzz"
+        
+        if(str === "") str += `${i}`
+        result.push(str) 
+    }
+    return result
 };
