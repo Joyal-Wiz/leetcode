dd40bac67fa3d42569ff33b98c48f64c0c25fdff
JOYAL JOSE <jhub0866@gmail.com>
2026-02-25 10:47:16 +0530

    Time: 42 ms (52.33%), Space: 64.9 MB (26.33%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.js
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
 var maxDistinct = function(s) {
-return new Set(s).size;
+let map ={};
+let count =0;
+for (let i=0;i<s.length;i++){
+    if(!map[s[i]]){
+        map[s[i]]=true;
+        count++;
+    }}
+    return count;
+
 };
