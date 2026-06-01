51bef40bec34393ae3f7bbb7669f12a7403f1f3f
JOYAL JOSE <jhub0866@gmail.com>
2026-06-01 18:31:25 +0530

    Time: 132 ms (62.79%), Space: 66.8 MB (65.12%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool AsteroidsDestroyed(
+        int mass,
+        int[] asteroids
+    ) {
+
+        Array.Sort(asteroids);
+
+        long currentMass = mass;
+
+        foreach (int asteroid in asteroids) {
+            if (currentMass < asteroid) {
+                return false;
+            }
+
+            currentMass += asteroid;
+        }
+
+        return true;
+    }
+}
