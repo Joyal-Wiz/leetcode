672d5f80aa2d71270db017b36ea656eac5b4df5f
JOYAL JOSE <jhub0866@gmail.com>
2026-06-02 08:29:13 +0530

    Time: 1 ms (100%), Space: 55.7 MB (100%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cs
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cs
@@ -1,7 +1,24 @@
 public class Solution {
-    public int EarliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) =>
-        Math.Min(
-            landStartTime.Select((m, i) => waterStartTime.Select((n, j) => m + landDuration[i] <= n ? n + waterDuration[j] : m + landDuration[i] + waterDuration[j]).Min()).Min(),
-            waterStartTime.Select((n, j) => landStartTime.Select((m, i) => n + waterDuration[j] <= m ? m + landDuration[i] : n + waterDuration[j] + landDuration[i]).Min()).Min()
-        );
+    private int solve(int[] start1, int[] duration1, int[] start2,
+                      int[] duration2) {
+        int finish1 = int.MaxValue;
+        for (int i = 0; i < start1.Length; i++) {
+            finish1 = Math.Min(finish1, start1[i] + duration1[i]);
+        }
+        int finish2 = int.MaxValue;
+        for (int i = 0; i < start2.Length; i++) {
+            finish2 =
+                Math.Min(finish2, Math.Max(start2[i], finish1) + duration2[i]);
+        }
+        return finish2;
+    }
+
+    public int EarliestFinishTime(int[] landStartTime, int[] landDuration,
+                                  int[] waterStartTime, int[] waterDuration) {
+        int land_water =
+            solve(landStartTime, landDuration, waterStartTime, waterDuration);
+        int water_land =
+            solve(waterStartTime, waterDuration, landStartTime, landDuration);
+        return Math.Min(land_water, water_land);
+    }
 }
