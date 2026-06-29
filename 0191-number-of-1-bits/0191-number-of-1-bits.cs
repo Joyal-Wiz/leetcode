d642b19eb68ffc1334fc1eb3aded1bd475e64fba
JOYAL JOSE <jhub0866@gmail.com>
2026-06-29 21:04:16 +0530

    Time: 1 ms (9.65%), Space: 28.7 MB (88.86%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int HammingWeight(int n) {
+        return BitCount(n);
+    }
+
+    private int BitCount(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+}
